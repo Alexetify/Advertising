311407375d9e568534715592a5de0af6ebf8fe40
alexetify_odessa <hhmh@rambler.ru>
2019-08-13 15:15:09 +0300

    gulpfile.js_almoust_done

--- www/gulpfile.js
+++ www/gulpfile.js
@@ -1,25 +1,34 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "www/"
-    });
-
-    gulp.watch("www/sass/*.sass", ['sass']);
-    gulp.watch("www/*.html").on('change', browserSync.reload);
-});
+var sass = require('gulp-sass');
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("www/sass/*.sass")
-        .pipe(sass())
-        .pipe(gulp.dest("www/css"))
-        .pipe(browserSync.stream());
+gulp.task('sass', function () {
+  return gulp.src('www/sass/*.sass')
+      .pipe(sass())
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('css'))
+      .pipe(browserSync.stream()); //{match: '**/*.css'}
 });
 
-gulp.task('default', ['serve']);
+
+function reload(done) {
+  browserSync.reload();
+  done();
+}
+
+// Server + watching scss/js files
+gulp.task('default', function () {
+  browserSync.init({
+    server: {
+                baseDir: "./"
+            }
+    
+  });
+  gulp.watch(['www/sass/*.sass'], gulp.parallel(reload));
+  gulp.watch(['www/*.html'], gulp.parallel(reload));
+  gulp.watch(['js/*.js'], gulp.parallel(reload));
+ 
+  
+});
+
